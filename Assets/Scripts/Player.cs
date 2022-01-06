c7262cf4af9fac4e95c99504cc2014afe3146409
markkuope <72963505+markkuope@users.noreply.github.com>
2022-01-06 16:04:40 +0200

    Properties

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,14 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        Score score = new Score();
+        //score.Point = 8;
+        //print(score.Point);
+        //score.Point = 3;
+        //print(score.Point);
+        score.Lives = 9;
+        print(score.Lives);
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,8 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Score : MonoBehaviour
-{  
+public class Score 
+{
+    private int point;
+    private int lives;
+
+    public int Lives { get; set; }
+
+    public int Point
+    {
+        get
+        {
+            return point; // palauttaa point – muuttujan arvon
+        }
+
+        set
+        {
+            if (value > 5 && value < 10)
+            {
+                point = value; // antaa point – muuttujalle arvon
+            }
+            PrintPoint();
+
+        }
+
+    }
+
+    void PrintPoint()
+    {
+        Debug.Log(point);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
