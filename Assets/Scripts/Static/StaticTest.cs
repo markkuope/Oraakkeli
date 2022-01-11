a7b804a55bfc0617a73df9dd9e3320c2a72a179b
markkuope <72963505+markkuope@users.noreply.github.com>
2022-01-11 17:48:33 +0200

    quit

--- Assets/Scripts/Static/StaticTest.cs
+++ Assets/Scripts/Static/StaticTest.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StaticTest : MonoBehaviour
-{    
+{
+
+    public static int PlayerCount; //Pelaajien lukum‰‰r‰
+
     void Start()
     {
-        Bullets bullet1 = new Bullets();
-        //Bullets.noOfBullets = 10;
+        PlayerCount++;
 
-        Bullets bullet2 = new Bullets();
-        //Bullets.noOfBullets = 20;
+        //Bullets bullet1 = new Bullets();
+        ////Bullets.noOfBullets = 10;
 
-        Bullets bullet3 = new Bullets();
+        //Bullets bullet2 = new Bullets();
+        ////Bullets.noOfBullets = 20;
 
-        print(Bullets.noOfBullets);
-        //print(bullet2.noOfBullets);
+        //Bullets bullet3 = new Bullets();
+
+        ////print(Bullets.noOfBullets);
+        ////print(bullet2.noOfBullets);
+
+        //Bullets.ShowBullets();
+
+        print("Pelaajien lukum‰‰r‰ on: " + PlayerCount);
+
     }
 
     // Update is called once per frame
@@ -32,6 +42,11 @@ public class Bullets
     public Bullets()  // constructor -funktio
     {
         noOfBullets++; // arvo kasvaa yhdell‰ aina kun muodostetaan uusi Bullets -muuttuja.
+    }
+
+    public static void ShowBullets()
+    {
+        Debug.Log("NO of Bullets :" + noOfBullets);
     }
 }
 
