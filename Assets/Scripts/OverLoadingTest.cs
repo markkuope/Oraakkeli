d55f466e6ac819d046913c6b618ec74c5b19f71b
markkuope <72963505+markkuope@users.noreply.github.com>
2022-01-13 17:32:15 +0200

    quit

---
+++ Assets/Scripts/OverLoadingTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverLoadingTest : MonoBehaviour
+{   
+    void Start()
+    {
+       
+    }
+   
+} // OverLoadingTest p‰‰ttyy t‰h‰n
