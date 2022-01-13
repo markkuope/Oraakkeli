e953e3bad78ab933d2691598877cf6a894aa2a17
markkuope <72963505+markkuope@users.noreply.github.com>
2022-01-13 16:34:36 +0200

    Update StaticTest.cs

--- Assets/Scripts/Static/StaticTest.cs
+++ Assets/Scripts/Static/StaticTest.cs
@@ -4,18 +4,54 @@ using UnityEngine;
 
 public class StaticTest : MonoBehaviour
 {
-   
+    public static int PlayerCount; //Pelaajien lukum‰‰r‰
+
+
     void Start()
     {
-       
+
+        PlayerCount++;
+
+        print(PlayerCount);
+
+
+        //Bullets bullet1 = new Bullets();
+        ////Bullets.noOfBullets = 10;
+
+        //Bullets bullet2 = new Bullets();
+        ////Bullets.noOfBullets = 20;
+
+        //Bullets bullet3 = new Bullets();
+
+        ////print(Bullets.noOfBullets);
+        ////print(bullet2.noOfBullets);
+
+        //Bullets.ShowBullets();
+        
     }
 
     void Update()
     {
-        
+       
     }
 } // t‰h‰n p‰‰ttyy StaticTest
 
+public class Bullets
+{
+    public static int noOfBullets = 0;
+
+    public Bullets()  // constructor -funktio
+    {
+        noOfBullets++; // arvo kasvaa yhdell‰ aina kun muodostetaan uusi Bullets -muuttuja.
+    }
+
+    public static void ShowBullets()
+    {
+        Debug.Log("NO of Bullets :" + noOfBullets);
+    }
+
+
+}
 
 
 
