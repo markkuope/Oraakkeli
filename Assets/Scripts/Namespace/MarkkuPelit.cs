693ecc9eeece1cf1064ef36915333f0240c30c5e
markkuope <72963505+markkuope@users.noreply.github.com>
2022-01-14 16:04:47 +0200

    namespacetestit

--- Assets/Scripts/Namespace/MarkkuPelit.cs
+++ Assets/Scripts/Namespace/MarkkuPelit.cs
@@ -1,2 +1,24 @@
 using UnityEngine;
 
+namespace MarkkuPelit //namespace -nimiavaruus
+{
+    public class Utilites  //class  -luokka
+    {
+        public static void PrintMarkku()  //method ñ metodi, funktio
+        {
+            Debug.Log("MarkkuPelit on parhaita!");
+        }
+    }
+
+    namespace UI
+    {
+        public class UIStuff
+        {
+            public static void DoUI()
+            {
+                Debug.Log(" Tehd‰‰n hieno User Interface");
+            }
+        }
+    }
+}
+
--- Assets/Scripts/Namespace/NameSpaceTest.cs
+++ Assets/Scripts/Namespace/NameSpaceTest.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MarkkuPelit;
+using MarkkuPelit.UI;
 
 public class NameSpaceTest : MonoBehaviour
 {    
     void Start()
     {
-       
+        //Utilites.PrintMarkku();
+        UIStuff.DoUI();
     }
 }
